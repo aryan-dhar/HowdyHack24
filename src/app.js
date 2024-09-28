93a49a787140543a27d77127042cabf1e145068f
aryan-dhar <aryand1010@gmail.com>
2024-09-28 14:48:17 -0500

    working code

--- src/app.js
+++ src/app.js
@@ -9,13 +9,16 @@ app.use(express.json());
 mongoose.connect('mongodb://localhost:27017/howdyhack24', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+})
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 
-const port = 3000;
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+app.use(express.static('publicFolder')); 
 
-app.use(express.static('publicFolder'));
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
