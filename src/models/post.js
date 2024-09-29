9aaa680a2c0d4b2e2565791959247d1f0bd095bd
aryan-dhar <aryand1010@gmail.com>
2024-09-29 10:48:31 -0500

    HowdyHelp 1.0

--- src/models/post.js
+++ src/models/post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
   message: { type: String, required: true },
-  category: { type: String, enum: ['anxiety', 'depression', 'stress', 'other'], default: 'other' },
+  category: { type: String, enum: ['Anxiety', 'Depression', 'Stress', 'Other'], default: 'Other' },
   timestamp: { type: Date, default: Date.now },
 });
 
